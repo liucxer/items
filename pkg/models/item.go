839e71f0ec6b2df67215f6a4e39d819f4899cdb2
SincosΣΩΔ <fangjian@rockontrol.com>
2022-01-05 18:39:22 +0800

    fix item.RichText database length limitation

--- pkg/models/item.go
+++ pkg/models/item.go
@@ -21,14 +21,14 @@ type ItemRef struct {
 }
 
 type ItemBase struct {
-	Code        string       `db:"f_code"                      json:"code"`                  // 条目代码
-	ParentCode  string       `db:"f_parent_code,default=''"    json:"parentCode,omitempty"`  // 条目上级代码
-	Name        string       `db:"f_name"                      json:"name"`                  // 条目名称
-	AlphabetZH  string       `db:"f_alphabet_zh,default=''"    json:"alphabetZH,omitempty"`  // 条目中文拼音
-	AlphabetEN  string       `db:"f_alphabet_en,default=''"    json:"alphabetEN,omitempty"`  // 条目英文
-	ImageResID  depends.SFID `db:"f_image_res_id,default='0'"  json:"imageResID,omitempty"`  // 条目icon资源ID
-	RichText    string       `db:"f_rich_text,default=''"      json:"richText,omitempty"`    // 条目富文本
-	Link        string       `db:"f_link,default=''"           json:"link,omitempty"`        // 条目跳转链接
-	AttachResID depends.SFID `db:"f_attach_res_id,default='0'" json:"attachResID,omitempty"` // 条目附件资源ID
-	HasSub      bool         `db:"f_has_sub,default='false'"   json:"hasSub,omitempty"`      // 是否有下一级条目
+	Code        string       `db:"f_code"                            json:"code"`                  // 条目代码
+	ParentCode  string       `db:"f_parent_code,default=''"          json:"parentCode,omitempty"`  // 条目上级代码
+	Name        string       `db:"f_name"                            json:"name"`                  // 条目名称
+	AlphabetZH  string       `db:"f_alphabet_zh,default=''"          json:"alphabetZH,omitempty"`  // 条目中文拼音
+	AlphabetEN  string       `db:"f_alphabet_en,default=''"          json:"alphabetEN,omitempty"`  // 条目英文
+	ImageResID  depends.SFID `db:"f_image_res_id,default='0'"        json:"imageResID,omitempty"`  // 条目icon资源ID
+	RichText    string       `db:"f_rich_text,default='',size=10240" json:"richText,omitempty"`    // 条目富文本
+	Link        string       `db:"f_link,default=''"                 json:"link,omitempty"`        // 条目跳转链接
+	AttachResID depends.SFID `db:"f_attach_res_id,default='0'"       json:"attachResID,omitempty"` // 条目附件资源ID
+	HasSub      bool         `db:"f_has_sub,default='false'"         json:"hasSub,omitempty"`      // 是否有下一级条目
 }
